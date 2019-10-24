2d607246c96d25bbcd54c0d31f34a013b0f16a1e
Miles Budden <miles@budden.net>
2019-10-24 17:36:29 +0100

    Added a cheeky script

---
+++ runall.sh
@@ -0,0 +1,12 @@
+java -Xmx200m rushhour/RushHour random1.txt &
+java -Xmx200m rushhour/RushHour random2.txt &
+java -Xmx200m rushhour/RushHour random3.txt &
+java -Xmx200m rushhour/RushHour random4.txt &
+java -Xmx500m rushhour/RushHour random5.txt &
+java -Xmx500m rushhour/RushHour random6.txt &
+java -Xmx200m rushhour/RushHour random7.txt &
+java -Xmx500m rushhour/RushHour random8.txt &
+java -Xmx500m rushhour/RushHour random9.txt &
+java -Xmx200m rushhour/RushHour random10.txt &
+java -Xmx500m rushhour/RushHour random11.txt &
+java -Xmx200m rushhour/RushHour random12.txt
